b79081291a6c6213c5c151ce383e7c39652dda68
Francisco Abarza <francisco@maskin.cl>
2018-02-11 18:54:36 -0300

    filtering guests

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import GuestList from "./GuestList"
 class App extends Component {
 
   state = {
+    ifFiltered: false,
     guests: [
       {
         name: "Treasure",
@@ -58,6 +59,9 @@ class App extends Component {
       })
     })
 
+  toggleFilter = () =>
+    this.setState({ isFiltered: !this.state.isFiltered})
+
   getTotalInvited = () => this.state.guest.length
   // getAttendingGuests = () =>
   // getUnconfirmedGuests = () =>
@@ -77,7 +81,10 @@ class App extends Component {
           <div>
             <h2>Invitees</h2>
             <label>
-              <input type="checkbox" /> Hide those who haven't responded
+              <input
+                type="checkbox" 
+                onChange={this.toggleFilter}
+                checked={this.state.isFiltered} /> Hide those who haven't responded
             </label>
           </div>
           <table className="counter">
@@ -100,7 +107,8 @@ class App extends Component {
             guests={this.state.guests} 
             toggleConfirmationAt={this.toggleConfirmationAt}
             toggleEditingAt={this.toggleEditingAt}
-            setNameAt={this.setNameAt} />
+            setNameAt={this.setNameAt}
+            isFiltered={this.state.isFiltered} />
         </div>
       </div>
     )
